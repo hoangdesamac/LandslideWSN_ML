e864192577aa587bc4e189190b135966c07bc434
hoangdesamac <tanthat2019@gmail.com>
2025-12-13 13:18:16 +0700

    iniatula

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+def main():
+    print('⛰️  Khởi động hệ thống dự báo sạt lở (LandslideWSN_ML)...')
+    # Code xử lý chính sẽ gọi ở đây
+
+if __name__ == '__main__':
+    main()
